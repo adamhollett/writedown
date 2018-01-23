42c888546f28fb6b111d7671b53a59d94b0730e7
Adam Hollett <mail@adamhollett.com>
2018-01-23 11:28:30 -0500

    Organize figure method

--- lib/writedown/figure.rb
+++ lib/writedown/figure.rb
@@ -13,23 +13,31 @@ module Writedown
       image = el.children.first
       figure.children << image
 
-      # Add a caption to the figure if needed
-      if Writedown.configuration.figure_captions && image.attr['title']
-        caption_text = Kramdown::Element.new(:text, image.attr['title'])
-        caption_element = Kramdown::Element.new(:html_element, 'figcaption', nil, category: :block)
-        caption_element.children << caption_text
-        image.attr['title'] = nil unless Writedown.configuration.figure_preserve_title
-
-        # Add the figure caption
-        case Writedown.configuration.figure_caption_position
-        when :above, 'above'
-          figure.children.unshift(caption_element)
-        else
-          figure.children << caption_element
-        end
+      # Add a caption to the figure
+      if Writedown.configuration.figure_captions && title?(image)
+        prepare_caption(figure, image)
       end
 
       format_as_indented_block_html('figure', figure.attr, inner(figure, indent), indent)
+    end
+
+    def prepare_caption(figure, image)
+      caption_text = Kramdown::Element.new(:text, image.attr['title'])
+      caption_element = Kramdown::Element.new(:html_element, 'figcaption', nil, category: :block)
+      caption_element.children << caption_text
+      image.attr['title'] = nil unless Writedown.configuration.figure_preserve_title
+
+      # Position the caption
+      case Writedown.configuration.figure_caption_position
+      when :above, 'above'
+        figure.children.unshift(caption_element)
+      else
+        figure.children << caption_element
+      end
+    end
+
+    def title?(image)
+      image.attr['title']
     end
   end
 end
